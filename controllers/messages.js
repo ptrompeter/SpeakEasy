db7e0cf894f9416b7e91df7f86f7c606ecdd2a1b
Alemneh <Alemneh@hotmail.com>
2016-01-26 00:26:30 -0800

    incorporated google translator api to translate messages in messages.js file

--- controllers/messages.js
+++ controllers/messages.js
@@ -1,11 +1,20 @@
 var express = require('express');
 var db = require('../models');
 var router = express.Router();
+var request = require('request');
+
+
+
+
+
+
+
 
 // Routes
 
 //Directs to message form
 router.get('/new', function(req,res){
+  console.log(res.b+' here 1');
   res.render('messages/new.ejs')
 });
 
@@ -13,19 +22,40 @@ router.get('/new', function(req,res){
 
 // Send New Message Route
 
+
+
 router.post('/new', function (req,res){
- db.user.find({where: {userName: req.body.pal}}).then(function(pal){
-   db.message.create({
-     body: req.body.text,
-     translation: 'LATER - When API call is working.',
-     userId: req.user.id,
-     palId: pal.id,
-     userName: req.user.userName,
-     palName: pal.userName
-   }).then(function(message){
-     res.send(message);
+
+//Variables for googleapis//
+  var apiKey = 'key=AIzaSyCCq7pSGvCgu5tcDJezGo82QIBe7R8hCX8&';
+  var from = 'en'; //source of language//
+  var to = 'ar'; //transating language//
+  var url = 'https://www.googleapis.com/language/translate/v2?q=';
+  var input = req.body.text; // grab text from new msg post and use it in query for api//
+  var translations;
+//api http request to googleapi//
+ request(url+input+'&source='+from+'&target='+to+'&'+apiKey, function (error, response, body) {
+     if (!error && response.statusCode == 200) {
+         var data =JSON.parse(body);
+         translations = data.data.translations[0].translatedText;
+
+     }
+ }).on('response', function(response) {
+     db.user.find({where: {userName: req.body.pal}}).then(function(pal){
+       db.message.create({
+         body: req.body.text,
+         translation: translations,
+         userId: req.user.id,
+         palId: pal.id,
+         userName: req.user.userName,
+         palName: pal.userName
+       }).then(function(message){
+
+         res.send(message);
+       });
+     });
    });
- });
+
 });
 
 
