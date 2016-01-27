6b6f04021ec4562648eecaa9d5970a25e82f7f5b
Andy Malik <me@andymalik.com>
2016-01-27 09:52:35 -0800

    Style guide added to css. Working on some persistence.

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,13 @@
 var express = require('express');
 var db = require('../models');
 var router = express.Router();
-
+var potentialpal;
+var sendORcanc;
+var done4day;
+var empty;
+var pending;
+var newPal;
+var accORrej;
 // Routes
 
 router.get('/pals', function(req, res){
@@ -9,30 +15,43 @@ router.get('/pals', function(req, res){
   var incl = [];
   db.usersPals.findAll({ where: {userId: req.user.id}}).then(function(pals){
     db.user.findAll({ where: {palRecipient: true, matchWaiting: false, id: {ne: req.user.id}}}).then(function(users){
-      var palsArray = pals.map(function(Pele, Pindex, Parray){
-        var userArray = users.map(function(Aele, Aindex, Aarray){
-          if (Pele.palId !== Aele.id) {
-            if (typeof excl[0] === 'undefined') {
-              if (incl.indexOf(Aele) === -1){
-                incl.push(Aele);
-              }
-            }
-            var exclmap = excl.map(function(Eele, Eindex, Earray){
-              if (Eele !== Aele.id) {
+      if (pals.length === 0) {
+        if (users.length === 0) {
+          res.render('users.ejs', {empty: true, pending: pending, newPal: newPal, accORrej: accORrej});
+        } else {
+          randNum = Math.floor(Math.random()*users.length);
+          potentialpal = users;
+          res.render('users.ejs', {potentialpal: users, randNum: randNum, pending: pending, newPal: newPal, accORrej: accORrej})
+        }
+      } else {
+        var palsArray = pals.map(function(Pele, Pindex, Parray){
+          var userArray = users.map(function(Aele, Aindex, Aarray){
+            if (Pele.palId !== Aele.id) {
+              if (typeof excl[0] === 'undefined') {
                 if (incl.indexOf(Aele) === -1){
                   incl.push(Aele);
                 }
+              } else {
+                var exclmap = excl.map(function(Eele, Eindex, Earray){
+                  if (Eele !== Aele.id) {
+                    if (incl.indexOf(Aele) === -1){
+                      incl.push(Aele);
+                    }
+                  }
+                });
               }
-            });
-          } else {
-            excl.push(Aele.id);
-          }
+            } else {
+              excl.push(Aele.id);
+            }
+          });
         });
-      });
-      if (incl.length===0){
-        res.render('users.ejs', {error: true});
-      } else {
-        res.render('users.ejs', {potentialpal: incl})
+        if (incl.length===0){
+          res.render('users.ejs', {empty: true, pending: pending, newPal: newPal, accORrej: accORrej});
+        } else {
+          randNum = Math.floor(Math.random()*incl.length);
+          potentialpal = incl;
+          res.render('users.ejs', {potentialpal: incl, randNum: randNum})
+        }
       }
     });
   });
@@ -40,14 +59,20 @@ router.get('/pals', function(req, res){
 
 router.post('/pals', function(req, res){
   if (req.body.palbutton==='NO'){
-    res.render('users.ejs', {sendORcanc: req.body.palbutton, potentialpal: ''});
+    res.render('users.ejs', {
+      sendORcanc: req.body.palbutton,
+      potentialpal: potentialpal
+    });
   } else {
     db.user.update({
       matchWaiting: true,
       sentBy: req.user.userName},
       {where: {userName: req.body.pal}}
     ).then(function(user){
-      res.render('users.ejs', {sendORcanc: req.body.palbutton, potentialpal: ''});
+      res.render('users.ejs', {
+        sendORcanc: req.body.palbutton,
+        potentialpal: potentialpal
+      });
     });
   }
 });
