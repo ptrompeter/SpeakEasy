a8e7e362ef371fa388f02b88354c7a95f9dba2f9
Alemneh <Alemneh@hotmail.com>
2016-01-29 07:31:30 -0800

    cleaned up coded and added comments to refactored codes

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -11,43 +11,44 @@ $(window).load(function() {
       weekday: "long", year: "numeric", month: "short",
       day: "numeric", hour: "2-digit", minute: "2-digit"
   };
-
+// chat object
   var chat = {}
 
-  chat.sendMessage = function(e) {
+// function to send messages to the server
+chat.sendMessage = function(e) {
 
   msg = $('#myMsg').val().trim();
-
   if(msg.length > 0) {
       var completeMsg = msg;
       console.log(completeMsg);
       socket.emit('msg', completeMsg);
     }
-  };
+};
+// date and time stamp for messages
+chat.date = date.toLocaleTimeString("en-us", options);
 
-  chat.date = date.toLocaleTimeString("en-us", options);
 
 
-      // console.log(to);
-
+// when send btn is clicked sent to socket.io on server
   $('#sendBtn').on('click', function(e) {
         e.preventDefault()
         chat.sendMessage();
   });
 
 
-
+// when connection is made to socket.io they are joined to a room
   socket.on('connect', function () {
     socket.emit('room', room);
   });
 
-
+// when a message is sent to the client from the server
   socket.on('msg', function(incomingMsg) {
       decoratedHtml = '<p class="msgfield"><b>'+userName + ':</b> '+ incomingMsg +'&nbsp;&nbsp;&nbsp;&nbsp;<small>'+chat.date+'</small></p>';
       $('#chatsContainer').append(decoratedHtml);
   });
 
+
   $('#hamMenu').on('click', function() {
       $('nav').slideToggle();
-   }); 
+   });
 });
